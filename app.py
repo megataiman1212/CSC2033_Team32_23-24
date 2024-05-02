c15cfa62d2da3d759922f17423f43a15ffabc185
DanielCook177 <c2027971@newcastle.ac.uk>
2024-05-02 12:58:15 +0100

    Created HTML files for all the web pages and created links to them

--- app.py
+++ app.py
@@ -6,3 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/database')
+def database():
+    return render_template('database.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
