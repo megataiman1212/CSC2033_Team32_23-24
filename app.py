0eb4f5d78f8d0a2ad07814541d82cbf3b36347a7
asha7494 <asha7494s@gmail.com>
2024-05-02 18:30:32 +0100

    added product table with basic values

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Warehouse.db'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
