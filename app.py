17101c475960b708a2b55b1680b63ee012b723bd
asha7494 <asha7494s@gmail.com>
2024-05-07 10:59:15 +0100

    removed old database as switching to mongo

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Warehouse.db'
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
 
 
 @app.route('/')
