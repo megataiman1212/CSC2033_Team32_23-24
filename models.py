416ccc57e9ee35f41657d8f6c57a82da6b5840d8
DanielCook177 <c2027971@newcastle.ac.uk>
2024-05-13 12:35:01 +0100

    Merged master and front end

--- models.py
+++ models.py
@@ -37,4 +37,7 @@ class Product(db.Model):
 #Run create_db file
 #Run models file
 with app.app_context():
+    db.drop_all()
     db.create_all()
+    db.session.add(User(email="admin@admin.com", password="admin123!", access_level="admin"))
+    db.session.commit()
