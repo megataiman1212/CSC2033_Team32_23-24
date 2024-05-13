d8a77993ea961b9d7896c3a4db7220853c8b8a60
Daniel-Ellam <d.ellam2@newcastle.ac.uk>
2024-05-13 13:48:31 +0100

    Add database and query views

--- database/views.py
+++ database/views.py
@@ -1,7 +1,27 @@
 from flask import Blueprint, render_template, request, redirect
+from database.forms import SearchForm
+import databaseControl
 
 database_blueprint = Blueprint('database', __name__, template_folder='templates')
 
+
 @database_blueprint.route('/database', methods=['GET', 'POST'])
-def admin():
-    return render_template('database/database.html')
+def database():
+    products = databaseControl.get_all_products()
+
+    return render_template('database/database.html', products=products)
+
+
+@database_blueprint.route('/database.query', methods=['GET', 'POST'])
+def query():
+    form = SearchForm()
+
+    if form.validate_on_submit():
+        search_string = form.search.data
+
+        products = databaseControl.query_products(search_string)
+
+        render_template('database/database.html', products=products)
+
+    # if string is invalid, full table is displayed
+    return database()
