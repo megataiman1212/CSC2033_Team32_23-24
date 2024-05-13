ba69a589d88b9b33d83defa0e9b8d2baaaf126fc
c220326740 <S.N.B.Azman2@newcastle.ac.uk>
2024-05-13 20:00:07 +0800

    add pseudocode

--- databaseview.py
+++ databaseview.py
@@ -1,1 +1,29 @@
 
+# FR8
+def get_all_products():
+    # return render_template
+    # database page
+
+# FR15
+def query_product(SearchString):
+    # return render_template
+    # database page
+
+
+# FR9
+def add_products(ProductID, Stock, Category, RequiredLevel):
+
+
+# FR10
+def increase_stock(ProductID):
+
+
+# FR10
+def reduce_stock(ProductID):
+
+# FR17
+def change_order_level(ProductID, NewNumber):
+
+# FR5
+def delete_product(ProductID):
+
