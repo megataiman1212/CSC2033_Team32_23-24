7c7cb5ebf37f6cadf36ee214c84ef51aa50465d2
c220326740 <S.N.B.Azman2@newcastle.ac.uk>
2024-05-13 23:13:44 +0800

    views function for add_admin

--- admin/views.py
+++ admin/views.py
@@ -6,3 +6,17 @@ admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
 @admin_blueprint.route('/admin', methods=['GET', 'POST'])
 def admin():
     return render_template('admin/admin.html')
+
+
+@admin_blueprint.route('/admin', methods=['GET', 'POST'])
+def add_admin():
+    # form = RegisterForm
+    if form.validate_on_submit():
+        # The same registration logic as above, but set the role to 'admin'
+        new_admin = User(email=form.email.data,
+                         password=form.password.data,
+                         role='admin')
+
+
+    return redirect(url_for('admin.admin'))
+
