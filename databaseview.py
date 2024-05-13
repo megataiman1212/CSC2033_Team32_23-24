39f2087fe547c09bd819c243f84b79a81b473abe
louis <c0007898@newcastle.ac.uk>
2024-05-13 17:15:31 +0100

    moved databaseview.py

--- databaseview.py
+++ databaseview.py
@@ -1,12 +1,12 @@
 
 # FR8
 def get_all_products():
-    # return render_template
+        #return render_template
     # database page
 
 # FR15
 def query_product(SearchString):
-    # return render_template
+        # return render_template
     # database page
 
 
@@ -14,11 +14,11 @@ def query_product(SearchString):
 def add_products(ProductID, Stock, Category, RequiredLevel):
 
 
-# FR10
+    # FR10
 def increase_stock(ProductID):
 
 
-# FR10
+    # FR10
 def reduce_stock(ProductID):
 
 # FR17
