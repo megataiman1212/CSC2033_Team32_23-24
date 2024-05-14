d6504779b9c36496728f1ef6c82c4505131b6f2e
asha7494 <asha7494s@gmail.com>
2024-05-14 11:24:46 +0100

    added validation to models.py

--- models.py
+++ models.py
@@ -9,6 +9,19 @@ class User(db.Model):
     access_level = db.Column(db.String(100), nullable=False)
 
     def __init__(self,email, password, access_level="user"):
+        # Validate email
+        if not isinstance(email, str):
+            raise TypeError("Email must be a string")
+
+        # Validate password
+        if not isinstance(password, str):
+            raise TypeError("Password must be a string")
+
+        # Validate access level
+        allowed_access_levels = ["user", "admin"]
+        if access_level not in allowed_access_levels:
+            raise ValueError("Access level must be 'user' or 'admin'")
+
         self.email = email
         self.password = password
         self.access_level = access_level
@@ -22,6 +35,23 @@ class Product(db.Model):
     required_level = db.Column(db.Integer)
 
     def __init__(self, product, stock, category, required_level):
+        #validate product
+        if not isinstance(product, str):
+            raise TypeError("Product must be a string")
+
+        #validate stock
+        if not isinstance(stock, int):
+            raise TypeError("Stock must be an integer")
+
+        #validate category
+        allowed_category = ["food", "hygiene"]
+        if category not in allowed_category:
+            raise ValueError("Category must be 'food' or 'hygiene'")
+
+        #validate required_level
+        if not isinstance(required_level, int):
+            raise TypeError("Required level must be an integer")
+
         self.product = product
         self.stock = stock
         self.category = category
