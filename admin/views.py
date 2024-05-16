9052e3cfa9f9f2984c2dc5dfe35362dfc07cc780
Daniel-Ellam <d.ellam2@newcastle.ac.uk>
2024-05-16 11:53:30 +0100

    Admin use DbManager

--- admin/views.py
+++ admin/views.py
@@ -1,10 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, get_flashed_messages
 
-import databaseControl
+from Database_Manager.db_crud import DbManager
 from admin.forms import RegisterForm
-from app import db
-from models import User
-from flask_login import current_user
+from flask_login import login_required
 
 admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
 
@@ -18,22 +16,16 @@ def admin():
 def add_admin():
     form = RegisterForm
     if form.validate_on_submit():
-        # The same registration logic as above, but set the access_level to 'admin'
-        new_admin = User(email=form.email.data,
-                         password=form.password.data,
-                         access_level='admin')
-        # add and commit new admin to the database
-        db.session.add(new_admin)
-        db.session.commit()
+        DbManager.add_staff(RegisterForm.email.data, RegisterForm.password.data, "admin")
 
     return redirect(url_for('admin.admin'))
 
 
-@admin_blueprint.route('/add_staff', methods=['GET','POST'])
+@admin_blueprint.route('/add_staff', methods=['GET', 'POST'])
 def add_staff():
     form = RegisterForm
     if form.validate_on_submit():
-        databaseControl.add_staff(form.email.data, form.password.data)
+        DbManager.add_staff(form.email.data, form.password.data, "staff")
 
     return redirect(url_for('admin.admin'))
 
@@ -41,14 +33,14 @@ def add_staff():
 @admin_blueprint.route('/delete_staff', methods=['POST'])
 def delete_staff():
 
-    databaseControl.delete_staff()
+    DbManager.delete_staff()
 
     return redirect(url_for('admin.admin'))
 
 
 @admin_blueprint.route('/get_staff_account', methods=['POST'])
 def get_staff_account():
-    accounts = databaseControl.get_all_users()
+    accounts = DbManager.get_all_users()
 
     return render_template('admin/account.html', accounts=accounts)
 
