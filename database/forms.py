fc2a7a643ee7998a10b1a5b1659e95c26f01875a
Daniel-Ellam <d.ellam2@newcastle.ac.uk>
2024-05-17 13:51:55 +0100

    Add add products

--- database/forms.py
+++ database/forms.py
@@ -1,8 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
 
 
-class SearchForm(FlaskForm):
-    number = StringField(validators=[DataRequired()])
+def is_real(form, stock):
+    if 0 > stock.data or stock.data > 99999:
+        raise ValidationError('Stock must be between 0 and 100000')
+
+
+def validate_category(form, category):
+    if category.data != "food" and category.data != "hygiene":
+        raise ValidationError('category must be "food" or "hygiene"')
+
+
+class ProductForm(FlaskForm):
+    name = StringField(validators=[DataRequired()])
+    stock = IntegerField(validators=[DataRequired(),
+                                     is_real])
+    category = StringField(validators=[DataRequired(),
+                                       validate_category])
+    required_stock = IntegerField(validators=[DataRequired(),
+                                              is_real])
     submit = SubmitField()
--- database/views.py
+++ database/views.py
@@ -2,6 +2,7 @@ from flask import request
 from Database_Manager.db_crud import DbManager
 from flask import Blueprint, render_template
 from flask_login import login_required
+from database.forms import ProductForm
 
 database_blueprint = Blueprint('database', __name__, template_folder='templates')
 
@@ -39,6 +40,21 @@ def adjust_stock(product_id, mode):
         results = []
 
     return render_template("database/database.html", results=results)
+
+
+@database_blueprint.route('/add_product', methods=['GET', 'POST'])
+def add_product():
+    form = ProductForm()
+
+    if form.validate_on_submit():
+        Db = DbManager()
+        Db.add_product(form.name.data,
+                       form.stock.data,
+                       form.category.data,
+                       form.required_stock.data)
+        return render_template("database/database.html")
+
+    return render_template('database/add_product.html', form=form)
 
 
 @database_blueprint.route('/<int:product_id>/delete_product', methods=['GET', 'POST'])
