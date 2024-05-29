b21d153f2daa974f7fb86daa1c92c9912e62aa08
c220326740 <S.N.B.Azman2@newcastle.ac.uk>
2024-05-30 06:02:18 +0800

    add comments

--- database/forms.py
+++ database/forms.py
@@ -1,19 +1,22 @@
-#File written by Daniel E, Asha
+# File written by Daniel E, Asha
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import InputRequired, ValidationError
 
 
 def is_real(form, number):
+    # Check if a number is within a specified range (0 to 99,999)
     if number.data < 0 or number.data > 99999:
         raise ValidationError('Number must be between 0 and 100000')
 
 
 def validate_category(form, category):
+    # Check if a category is either "food" or "hygiene"
     if category.data.lower() != "food" and category.data.lower() != "hygiene":
         raise ValidationError('category must be "food" or "hygiene"')
 
 
+# Create form class for adding products
 class ProductForm(FlaskForm):
     name = StringField(validators=[InputRequired()])
     stock = IntegerField(validators=[InputRequired(),
@@ -25,7 +28,7 @@ class ProductForm(FlaskForm):
     submit = SubmitField()
 
 
+# Create form class for required stock
 class RequiredStockForm(FlaskForm):
-    new_level = IntegerField(validators=[InputRequired(),
-                                              is_real])
+    new_level = IntegerField(validators=[InputRequired(), is_real])
     submit = SubmitField()
